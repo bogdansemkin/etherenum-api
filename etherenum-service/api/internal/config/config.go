310b5466ee3d815a2abf18a71da3ebdc71dbdcca
Bogdan Donchenko <40574816+goddamityouscrewedupagain@users.noreply.github.com>
2022-10-13 23:14:32 +0300

    feat: add project structure, conn to db

---
+++ etherenum-service/api/internal/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+type (
+	Config struct {
+		Postgres
+		Mongo
+		HTTP
+		RabbitMQ
+	}
+
+	Postgres struct {
+		User     string `env:"POSTGRESQL_USER" env-default:"postgres"`
+		Password string `env:"POSTGRESQL_PASSWORD" env-default:"postgres"`
+		Host     string `env:"POSTGRESQL_HOST" env-default:"127.0.0.1"`
+		Database string `env:"POSTGRESQL_DATABASE" env-default:"chat_api"`
+	}
+
+	Mongo struct {
+		Name string `env:"MONGODB_NAME" env-default:"mongodb"`
+		Host string `env:"MONGODB_HOST" env-default:"localhost"`
+		Port string `env:"MONGODB_PORT" env-default:"27017"`
+	}
+
+	HTTP struct {
+		Port                       string `env:"HTTP_PORT" env-default:"8080"`
+		SendDetailsOnInternalError bool   `env:"HTTP_SEND_DETAILS_ON_INTERNAL_ERROR" env-default:"true"`
+	}
+
+	RabbitMQ struct {
+		User     string `env:"RABBITMQ_USER" env-default:"guest"`
+		Password string `env:"RABBITMQ_PASSWORD" env-default:"guest"`
+		Host     string `env:"RABBITMQ_HOST" env-default:"localhost"`
+		Port     string `env:"RABBITMQ_USER" env-default:"5672"`
+	}
+)
---
+++ etherenum-service/api/pkg/database/mongo.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoDBConfig struct {
+	Name string
+	Port string
+	Host string
+}
+
+type MongoDB struct {
+	*mongo.Collection
+}
+
+func NewMongo(config MongoDBConfig) (*mongo.Collection, error) {
+	opt := options.Client().ApplyURI(fmt.Sprintf("%s://%s:%s", config.Name, config.Host, config.Port))
+	client, err := mongo.NewClient(opt)
+	if err != nil {
+		return nil, fmt.Errorf("error during connection to mongo db: %s", err)
+	}
+
+	err = client.Connect(context.TODO())
+	if err != nil {
+		return nil, fmt.Errorf("error during client connection to mongo db: %s", err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error during ping : %s", err)
+	}
+	database := client.Database("etherenum-api")
+	transactionsCollection := database.Collection("transactions")
+
+	return transactionsCollection, nil
+}
