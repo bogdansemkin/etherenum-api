b5dfe6fa0102f4a84371b250395529907026d4c0
Bogdan Donchenko <40574816+goddamityouscrewedupagain@users.noreply.github.com>
2022-10-20 11:41:48 +0300

    fix: change port

--- etherenum-service/api/internal/cmd/main.go
+++ etherenum-service/api/internal/cmd/main.go
@@ -5,16 +5,19 @@ import (
 	"etherenum-api/etherenum-service/api/internal/config"
 	"fmt"
 	"github.com/ilyakaznacheev/cleanenv"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	var cfg config.Config
+
 	err := cleanenv.ReadEnv(&cfg)
 	if err != nil {
 		fmt.Printf("failed to read env, %s", err)
 	}
 
-	err = app.Run(&cfg)
+	err = app.Run(&cfg, port)
 	if err != nil {
 		fmt.Printf("error during start app, %s", err)
 	}
